3e4c0cd6dae7159951192ba6ef78e8f65126ee22
BROUSSARD Tim <tim.broussard@etu.umontpellier.fr>
2024-09-06 09:04:01 +0200

    feat : implémentation de la fonction clear dans la classe document

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -22,6 +22,7 @@ public class CommandeFactory {
             case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "majuscules" : return new CommandeMajuscule(document, parameters);
             case "effacer" : return new CommandeEffacer(document, parameters);
+            case "clear" : return new CommandeClear(document, parameters);
             default: return null;
 
         }
--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -37,4 +37,7 @@ public class Document {
     public void effacer(int debut, int fin) {
         remplacer(debut, fin, "");
     }
+    public void clear() {
+        texte = "c";
+    }
 }

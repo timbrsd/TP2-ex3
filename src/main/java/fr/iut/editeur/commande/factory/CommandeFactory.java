c21f73a1ed4c056399fb2c993ec5e5dbfa3db04b
BROUSSARD Tim <tim.broussard@etu.umontpellier.fr>
2024-09-06 08:59:11 +0200

    feat : implémentation de la fonction effacer dans la classe document

--- src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
+++ src/main/java/fr/iut/editeur/commande/factory/CommandeFactory.java
@@ -21,6 +21,7 @@ public class CommandeFactory {
             case "ajouter" : return new CommandeAjouter(document, parameters);
             case "remplacer" : return new CommandeRemplacer(document, parameters);
             case "majuscules" : return new CommandeMajuscule(document, parameters);
+            case "effacer" : return new CommandeEffacer(document, parameters);
             default: return null;
 
         }
